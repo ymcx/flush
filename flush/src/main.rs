4bb1ffed56973274d87cd03b3a9e2a78960cd54b
ymcx <89810988+ymcx@users.noreply.github.com>
2025-10-18 21:41:07 +0300

    hide the default time that is displayed in the prompt

--- flush/src/main.rs
+++ flush/src/main.rs
@@ -1,11 +1,14 @@
 use common::{self};
-use reedline::{DefaultPrompt, Reedline};
+use reedline::{DefaultPrompt, DefaultPromptSegment, Reedline};
 
 mod handle;
 
 fn main() {
     let mut reedline = Reedline::create();
-    let prompt = DefaultPrompt::default();
+    let prompt = DefaultPrompt::new(
+        DefaultPromptSegment::WorkingDirectory,
+        DefaultPromptSegment::Empty,
+    );
     let path_files = common::get_path_files();
 
     while handle::command(&mut reedline, &prompt, &path_files) {}
